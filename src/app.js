2fd7f9bec64ef06c549b02bae7cd95700972d910
jfss1 <jose.filipe.sampaio@live.com.pt>
2021-05-02 15:58:52 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ let game = io.connect("http://localhost:0007/games");
 game.on("welcome", (msg) =>{
     console.log("Received: ", msg);
 });
-game.emit("joinRoom", "Room1");
+game.emit("joinRoom", "PC");
 game.on("newUser", (res)=> console.log(res));
 game.on("err", (err) => console.log(err));
 game.on("success", (res) => console.log(res)); 
