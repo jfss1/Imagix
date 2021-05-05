18358f7958c370239c3355b1f7a15639b6d819c7
Andreia585 <83502265+Andreia585@users.noreply.github.com>
2021-05-05 10:13:02 +0100

    Create imagens.js

---
+++ src/imagens/imagens.js
@@ -0,0 +1,17 @@
+//imagens do jogo
+var imagesJson = {"items":[{
+    "id": 1,
+    "img": 'https://static.biologianet.com/conteudo/images/no-abacaxi-parte-comestivel-fruta-desenvolveu-se-diversas-pecas-florais-os-frutos-sao-cada-escama-casca-58f62e2a3fdfd.jpg'
+}, {
+    "id": 2,
+    "img": 'https://img.pokemondb.net/artwork/large/eevee-lets-go.jpg'
+},
+{
+    "id": 3,
+    "img": 'https://vigiadepreco.com.br/blog/wp-content/uploads/2020/10/Dicas-para-montar-um-PC-Gamer-top-de-linha-3.jpg'
+},
+{
+    "id": 4,
+    "img": 'https://i.pinimg.com/originals/fd/fa/80/fdfa8015ce2d8428ac34dc8e26f7982f.jpg'
+}
+]}
