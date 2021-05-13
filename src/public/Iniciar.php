f01c5c7ce678511eaee7ce98d72b407311dee0a5
Andreia585 <83502265+Andreia585@users.noreply.github.com>
2021-05-13 13:02:45 +0100

    Create Iniciar.php

---
+++ src/public/Iniciar.php
@@ -0,0 +1,19 @@
+<?php
+    //informaçao para obter a informaçao de json
+    $file = 'json/imagem.json';
+    $json = file_get_contents($file,0,null,null); 
+    $objs = json_decode($json,true);
+    
+    foreach ($objs as $value) {
+
+        //mostra a informaçao em array e conversao para as imagens
+        echo $value['solution'];
+        echo '<img src="data:img/png;base64,'.base64_encode($value['path']); echo '" >';
+
+        // exemplo que vi num site (teste com uma imagem da internet) -> se quiseres podes tirar so deixei para veres
+        $image = "http://qrfree.kaywa.com/?l=1&s=8&d=google.com";
+        $imageData = base64_encode(file_get_contents($image));
+        $src = 'data:image/png;base64,'.$imageData;
+        echo '<img src="'.$src.'">';
+    }
+?>
