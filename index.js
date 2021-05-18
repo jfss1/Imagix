095094ac1a8b571930a9f1c90d8a32a5c9149bad
jfss1 <jose.filipe.sampaio@live.com.pt>
2021-05-18 14:56:36 +0100

    mudar a porta e remoção do que nao precisa do index.html
    porta 8080 está ocupado para no meu pc

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 
 // Create a Node.js based http server on port 8080
-var server = require('http').createServer(app).listen(process.env.PORT || 8080);
+var server = require('http').createServer(app).listen(process.env.PORT || 7000);
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
